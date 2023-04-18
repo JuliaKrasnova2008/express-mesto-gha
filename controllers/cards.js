8bf1c622bb7895e7ecd26cfdecc5fb3ebe02c2ed
Julia Krasnova <ul1ya2008@yandex.ru>
2023-04-18 12:52:53 +0300

    проверка 7

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,30 +1,5 @@
 const Card = require('../models/card');
-
-module.exports.getCards = (req, res) => {
-  Card.find({})
-    .then((cards) => res.send(cards))
-    .catch(() => {
-      res.status(500).send({ message: 'Произошла ошибка на сервере.' });
-    });
-};
-
-module.exports.deleteCard = (req, res) => {
-  const { cardId } = req.params;
-
-  Card.deleteOne({ _id: cardId })
-    .then((card) => {
-      if (card.deletedCount === 0) {
-        return res.status(404).send({ message: 'Карточка не найдена' });
-      }
-      return res.send({ message: 'Карточка удалена' });
-    })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        return res.status(400).send({ message: 'Неправильно передан id.' });
-      }
-      return res.status(500).send({ message: 'Произошла ошибка на сервере.' });
-    });
-};
+const NotFound = require('../errors/errors');
 
 module.exports.addCard = (req, res) => {
   const { name, link } = req.body;
@@ -37,7 +12,7 @@ module.exports.addCard = (req, res) => {
     .catch((error) => {
       if (error.name === 'ValidationError') {
         res.status(400).send({
-          message: 'Передены невалидные данные.',
+          message: 'Переданы некорректные данные при создании карточки.',
         });
       } else {
         res.status(500).send({
@@ -47,21 +22,56 @@ module.exports.addCard = (req, res) => {
     });
 };
 
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => {
+      res.send({ cards });
+    })
+    .catch(() => {
+      res.status(500).send({ message: 'Произошла ошибка на сервере.' });
+    });
+};
+
+module.exports.deleteCard = (req, res) => {
+  const { cardId } = req.params;
+
+  Card.findByIdAndRemove({ _id: cardId })
+    .orFail(() => {
+      throw new NotFound();
+    })
+    .then((card) => {
+      res.send({ card });
+    })
+    .catch((error) => {
+      if (error.name === 'NotFound') {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+      } else if (error.name === 'CastError') {
+        res.status(400).send({ message: 'Неправильно передан id.' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка на сервере.' });
+      }
+    });
+};
+
 module.exports.likeCard = (req, res) => {
   const id = req.user._id;
   const { cardId } = req.params;
 
   Card.findByIdAndUpdate(cardId, { $pull: { likes: id } }, { new: true, runValidators: true })
+    .orFail(() => {
+      throw new NotFound();
+    })
     .then((card) => {
-      if (card) {
-        return res.send(card);
-      }
-      return res.status(404).send({ message: 'Карточка не найдена' });
-    }).catch((error) => {
-      if (error.name === 'CastError') {
+      res.send({ card });
+    })
+    .catch((error) => {
+      if (error.name === 'NotFound') {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+      } else if (error.name === 'CastError') {
         res.status(400).send({ message: 'Неправильно передан id.' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка на сервере.' });
       }
-      res.status(500).send({ message: 'Произошла ошибка на сервере.' });
     });
 };
 
@@ -70,15 +80,22 @@ module.exports.dislikeCard = (req, res) => {
   const { cardId } = req.params;
 
   Card.findByIdAndUpdate(cardId, { $pull: { likes: id } }, { new: true, runValidators: true })
+    .orFail(() => {
+      throw new NotFound();
+    })
     .then((card) => {
       if (card) {
         return res.send(card);
       }
       return res.status(404).send({ message: 'Карточка не найдена' });
-    }).catch((error) => {
-      if (error.name === 'CastError') {
+    })
+    .catch((error) => {
+      if (error.name === 'NotFound') {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+      } else if (error.name === 'CastError') {
         res.status(400).send({ message: 'Неправильно передан id.' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка на сервере.' });
       }
-      res.status(500).send({ message: 'Произошла ошибка на сервере.' });
     });
 };
