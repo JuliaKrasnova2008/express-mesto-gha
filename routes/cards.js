cc5011b1e6f2014f9a27717662e72f1f73e8ee16
Julia Krasnova <ul1ya2008@yandex.ru>
2023-04-19 11:20:22 +0300

    проверка 12

--- routes/cards.js
+++ routes/cards.js
@@ -1,24 +1,4 @@
-// const express = require('express');
-// const {
-//   getCards,
-//   deleteCard,
-//   addCard,
-//   likeCard,
-//   dislikeCard,
-// } = require('../controllers/cards');
-
-// const cardsRouter = express.Router();
-
-// cardsRouter.get('/', getCards);
-// cardsRouter.post('/', addCard);
-// cardsRouter.delete('/:cardId', deleteCard);
-// cardsRouter.put('/:cardId/likes', likeCard);
-// cardsRouter.delete('/:cardId/likes', dislikeCard);
-
-// module.exports = cardsRouter;
-
-const cardRoutes = require('express').Router();
-
+const express = require('express');
 const {
   getCards,
   deleteCard,
@@ -27,9 +7,12 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
-cardRoutes.get('/', getCards);
-cardRoutes.post('/', addCard);
-cardRoutes.delete('/:cardId', deleteCard);
-cardRoutes.put('/:cardId/likes', likeCard);
-cardRoutes.delete('/:cardId/likes', dislikeCard);
-module.exports = cardRoutes;
+const cardsRouter = express.Router();
+
+cardsRouter.get('/', getCards);
+cardsRouter.post('/', addCard);
+cardsRouter.delete('/:cardId', deleteCard);
+cardsRouter.put('/likes/:cardId', likeCard);
+cardsRouter.delete('/:cardId/likes', dislikeCard);
+
+module.exports = cardsRouter;
