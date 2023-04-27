23a72f14747150e2315c1d5a4005fd6426735adc
Julia Krasnova <ul1ya2008@yandex.ru>
2023-04-27 18:25:08 +0300

    ПР14 проверка 11

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,30 +1,23 @@
 const jwt = require('jsonwebtoken');
+const Unauthorized = require('../errors/unauthorized');
 
-const { unauthorized } = require('../errors/errorCodes');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
-const handleAuthError = (res) => {
-  res
-    .status(unauthorized)
-    .send({ message: 'Необходима авторизация' });
-};
-
-const extractBearerToken = (header) => header.replace('Bearer', '');
-
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer')) {
-    return handleAuthError(res);
+    throw new Unauthorized('Необходима авторизация!');
   }
 
-  const token = extractBearerToken(authorization);
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    return handleAuthError(res);
+    next(new Unauthorized('Необходима авторизация!'));
+    return;
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
