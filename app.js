a86ed721bcc5d9f757d0d8e00c2d5f0d2fcd6aba
Julia Krasnova <ul1ya2008@yandex.ru>
2023-04-28 13:16:54 +0300

    ПР14 проверка 21

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
+const { errors, celebrate, Joi } = require('celebrate');
 const helmet = require('helmet');
 const router = require('./routes');
 const { login, addUser } = require('./controllers/users');
-const { validateAddUser, validateLogin } = require('./middlewares/validation');
+const { REGEXP } = require('./middlewares/validation');
 const auth = require('./middlewares/auth');
 const defaultErr = require('./errors/defaultErr');
 
@@ -15,8 +15,35 @@ app.use(helmet());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', validateLogin, login);
-app.post('/signup', validateAddUser, addUser);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        email: Joi.string().required().email(),
+        password: Joi.string().required(),
+      })
+      .unknown(true),
+  }),
+  login
+);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        name: Joi.string().min(2).max(30),
+        about: Joi.string().min(2).max(30),
+        avatar: Joi.string().regex(REGEXP),
+        email: Joi.string().required().email(),
+        password: Joi.string().required(),
+      })
+      .unknown(true),
+  }),
+  addUser
+);
+// app.post('/signin', validateLogin, login);
+// app.post('/signup', validateAddUser, addUser);
 
 app.use(auth);
 app.use(router);
