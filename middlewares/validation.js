7fa3c33d373b77b96c1615d7bd8379304910631c
Julia Krasnova <ul1ya2008@yandex.ru>
2023-04-28 12:39:17 +0300

    ПР14 проверка 17

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -24,6 +24,8 @@ module.exports.validateAddUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }).unknown(true),
 });
 
