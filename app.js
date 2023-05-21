d2fb265d46aaf9019762a17a1360a121ba4619ff
Julia Krasnova <ul1ya2008@yandex.ru>
2023-05-21 21:40:52 +0400

    port 3000

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const NotFound = require('./errors/notFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const authRouter = require('./routes/auth');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
