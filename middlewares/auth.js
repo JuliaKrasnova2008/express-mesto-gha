d6ac66fbb865bfd038b762eab36e06ec243ba810
Julia Krasnova <ul1ya2008@yandex.ru>
2023-05-22 15:28:34 +0400

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer')) {
-    return next(new Unauthorized('Необходима Ваша авторизация!'));
+    return next(new Unauthorized('Необходима авторизация!'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -21,7 +21,7 @@ module.exports = (req, res, next) => {
       NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
     );
   } catch (err) {
-    return next(new Unauthorized('Необходима Ваша авторизация!'));
+    return next(new Unauthorized('Необходима авторизация!'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
