e4a06114bdcbe6a3ab7c3ffe7656b2293fe4c717
Julia Krasnova <ul1ya2008@yandex.ru>
2023-05-22 14:16:26 +0400

    cors

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const authRouter = require('./routes/auth');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1/mestodb');
 
 // app.use(cors({ origin: '*', optionsSuccessStatus: 200, }));
 app.use(helmet());
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
+app.use(cors);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
